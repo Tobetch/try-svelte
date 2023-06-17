b08aab8b2ebf361cdf91c0361671e5cb266eae6b
Tobetch <takutobe@gmail.com>
2023-06-17 21:16:28 +0900

    add code sample page

---
+++ demo-app/src/routes/sample/+page.svelte
@@ -0,0 +1,29 @@
+<!-- https://svelte.dev/tutorial/updating-arrays-and-objects -->
+<script>
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+
+	let numbers = [1, 2, 3, 4];
+
+	function addNumber() {
+		// numbers = [...numbers, numbers.length + 1];
+		numbers[numbers.length] = numbers.length + 1;
+	}
+	$: sum = numbers.reduce((t, n) => t + n, 0);
+</script>
+
+<section>
+	<button on:click={handleClick}>
+		Clicked {count}
+		{count === 1 ? 'time' : 'times'}
+	</button>
+
+	<div>
+		<p>{numbers.join(' + ')} = {sum}</p>
+
+		<button on:click={addNumber}> Add a number </button>
+	</div>
+</section>
